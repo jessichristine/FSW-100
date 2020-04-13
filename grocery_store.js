e888e5ff6c4be15e19f004d1fbaa158f7731fc16
jessichristine <63561021+jessichristine@users.noreply.github.com>
2020-04-12 20:14:28 -0400

    Add files via upload

---
+++ grocery_store.js
@@ -0,0 +1,11 @@
+var shopper = {
+    name: "Jessica Gray",
+    age: 43,
+    foodColor: ["steak", "salad", "cheese"],
+    likesToShop: true,
+    varShopper: function(){
+        return this.name + " " + this.age;
+    }    
+};
+    console.log(shopper.varShopper());
+
